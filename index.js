976df766e3b2aa40639fbf24417946b22255ff5a
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2023-10-18 22:59:19 +0600

    bmw and audi data get from database

--- index.js
+++ index.js
@@ -25,6 +25,57 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const carCollection = client.db('carsDB').collection('cars');
+
+    app.post('/car', async(req, res) => {
+      const car = req.body;
+      const result = await carCollection.insertOne(car);
+      res.send(result);
+    })
+
+    app.get('/bugatti', async(req, res) => {
+      const query = {brand: "BUGATTI"}
+      const bugatti = carCollection.find(query);
+      const result = await bugatti.toArray();
+      res.send(result);
+    })
+
+    app.get('/audi', async(req, res) => {
+      const query = {brand: "AUDI"}
+      const audi = carCollection.find(query);
+      const result = await audi.toArray();
+      res.send(result);
+    })
+
+    app.get('/bmw', async(req, res) => {
+      const query = {brand: "BMW"}
+      const bmw = carCollection.find(query);
+      const result = await bmw.toArray();
+      res.send(result);
+    })
+    
+    app.get('/lamborghini', async(req, res) => {
+      const query = {brand: "LAMBORGHINI"}
+      const lamborghini = carCollection.find(query);
+      const result = await lamborghini.toArray();
+      res.send(result);
+    })
+    app.get('/ferrari', async(req, res) => {
+      const query = {brand: "FERRARI"}
+      const ferrari = carCollection.find(query);
+      const result = await ferrari.toArray();
+      res.send(result);
+    })
+
+    app.get('/rollsroyce', async(req, res) => {
+      const query = {brand: "ROLLS-ROYCE"}
+      const rollsroyce = carCollection.find(query);
+      const result = await rollsroyce.toArray();
+      res.send(result);
+    })
+
+  
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
